c34b703e59d7b30ebd72f99030d30a19dd2f6570
Gauthier Matombi <gauthiermatombi@gmail.com>
2022-09-14 05:46:57 +0000

    resolved conflict

--- deploy.py
+++ deploy.py
@@ -1,6 +1,12 @@
 # Deploying a python web application
 # Shell script
+<<<<<<< HEAD
 #echo "Hello World"
 #Python script
 
 print('Hello World')
+=======
+echo "Hello World"
+#Python script
+python ('Hello World')
+>>>>>>> e0386b1 (deployment file added)
